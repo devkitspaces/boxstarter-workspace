12409ea5e785982a444484b9441c8b48a523ad1a
jrbeverly <jrbeverly@live.com>
2018-10-29 14:41:53 -0400

    Copy in the common tooling

---
+++ src/dev.tools/tools/ChocolateyInstall.ps1
@@ -0,0 +1,25 @@
+$Package = "jrbeverly.dev.tools"
+
+try {
+    $Boxstarter.RebootOk=$true
+    $Boxstarter.NoPassword=$false
+    $Boxstarter.AutoLogin=$true
+
+    Enable-RemoteDesktop
+
+    Disable-InternetExplorerESC
+    Disable-UAC
+
+    Update-ExecutionPolicy Unrestricted
+    Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+    Set-TaskbarOptions -Size Small -Lock -Dock Bottom
+    if (Test-PendingReboot) { Invoke-Reboot }
+
+    Install-WindowsUpdate -AcceptEula
+    if (Test-PendingReboot) { Invoke-Reboot }
+
+    Write-ChocolateySuccess $Package
+} catch {
+  Write-ChocolateyFailure $Package $($_.Exception.Message)
+  throw
+}
